a790a6a3034affc95526c34855e08572cd0c66c9
unknown <emgcalangian@gmail.com>
2024-01-25 09:56:41 +0800

    pseudo code

---
+++ raise_to_the_power.py
@@ -0,0 +1,9 @@
+# Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+
+# pseudo code
+
+# user input for base and exponent numbers
+
+# make a function and inside it use for loop to raise the base to the power of the exp
+# make an empty list for append
+# call the function
