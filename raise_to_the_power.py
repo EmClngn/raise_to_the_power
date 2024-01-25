c1c6d0f5505de545ee1763fcd5036c2f82819e7c
unknown <emgcalangian@gmail.com>
2024-01-25 10:21:02 +0800

    not working

--- raise_to_the_power.py
+++ raise_to_the_power.py
@@ -3,10 +3,17 @@
 # pseudo code
 
 # user input for base and exponent numbers
-input_base = input("What is your desired base number?: ")
-input_exp = input(f"Raise {input_base} to the power of what?: ")
-print(input_base)
-print(input_exp)
+input_base = int(input("What is your desired base number?: "))
+input_exp = int(input(f"Raise {input_base} to the power of what?: "))
+products = []
 # make a function and inside it use for loop to raise the base to the power of the exp
 # make an empty list for append
 # call the function
+def exponent(base, exp):
+    for _ in range(exp + 1):
+        base *= exp
+        products.append(base)
+    print(products[-1])
+
+exponent(input_base, input_exp)
+
