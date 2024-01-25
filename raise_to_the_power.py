a89d7c04687b59cfe0f4ec9a3b93c5a8e87e4ce3
unknown <emgcalangian@gmail.com>
2024-01-25 10:01:17 +0800

    user input done

--- raise_to_the_power.py
+++ raise_to_the_power.py
@@ -3,7 +3,10 @@
 # pseudo code
 
 # user input for base and exponent numbers
-
+input_base = input("What is your desired base number?: ")
+input_exp = input(f"Raise {input_base} to the power of what?: ")
+print(input_base)
+print(input_exp)
 # make a function and inside it use for loop to raise the base to the power of the exp
 # make an empty list for append
 # call the function
